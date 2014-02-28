49fc5951de7697a8887235b1c3b95456249ed793
Andrew Ahn <aahn@thrillist.com>
2014-02-28 18:37:08 -0500

    new game code

--- public/js/main.js
+++ public/js/main.js
@@ -6,10 +6,7 @@ function preload() {
 
     game.load.image('sky', '/images/sky2.png');
     game.load.image('ground', '/images/platform.png');
-    game.load.image('star', '/images/star.png');
-    game.load.spritesheet('dude', '/images/dude.png', 32, 48);
-
-    game.load.audio('boden', ['/audio/budligt_real_chinese.mp3']);
+    game.load.spritesheet('beerkeg', '/images/beerkeg.png', 24, 21);
 
 }
 
@@ -23,13 +20,18 @@ var scoreText;
 
 function create() {
 
+    background = game.add.graphics(0, 0);
+    background.beginFill(0x87CEEB, 1);
+    background.drawRect(0, 0, game.world.width, game.world.height);
+    background.endFill();
+
     music = game.add.audio('boden');
     //music.play();
 
     //  A simple background for our game
     //game.add.sprite(0, 0, 'sky');
 
-    bgtile = game.add.tileSprite(0, 0, game.stage.bounds.width, game.cache.getImage('sky').height, 'sky');
+    //bgtile = game.add.tileSprite(0, 0, game.stage.bounds.width, game.cache.getImage('sky').height, 'sky');
 
     //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = game.add.group();
@@ -43,54 +45,18 @@ function create() {
     //  This stops it from falling away when you jump on it
     ground.body.immovable = true;
 
-    //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
-    ledge.body.immovable = true;
-
-    ledge = platforms.create(-150, 250, 'ground');
-    ledge.body.immovable = true;
-
     // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
+    beerkeg = createBeerKeg();
 
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 1000;
-    player.body.collideWorldBounds = true;
+    game.physics.collide(beerkeg, ground);
 
-    //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-    /*
-    //  Finally some stars to collect
-    stars = game.add.group();
-
-    //  Here we'll create 12 of them evenly spaced apart
-    for (var i = 0; i < 12; i++)
-    {
-        //  Create a star inside of the 'stars' group
-        var star = stars.create(i * 70, 0, 'star');
-
-        //  Let gravity do its thing
-        star.body.gravity.y = 1200;
-
-        //  This just gives each star a slightly random bounce value
-        star.body.bounce.y = 0.7 + Math.random() * 0.2;
-    }
-*/
-    //  The score
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-
-    //  Our controls.
-    cursors = game.input.keyboard.createCursorKeys();
     
 }
 
 function update() {
 
-     bgtile.tilePosition.x -= 10;
-
+     //bgtile.tilePosition.x -= 10;
+/*
     //  Collide the player and the stars with the platforms
     game.physics.collide(player, platforms);
     game.physics.collide(stars, platforms);
@@ -128,18 +94,24 @@ function update() {
     {
         player.body.velocity.y = -650;
     }
-
+*/
 }
 
-function collectStar (player, star) {
-    
-    // Removes the star from the screen
-    star.kill();
+function createBeerKeg() {
 
-    //  Add and update the score
-    score += 10;
-    scoreText.content = 'Score: ' + score;
+    beerkeg = game.add.sprite(0, 0, 'beerkeg');
+    beerkeg.anchor.setTo(0.5, 0.5);
+    beerkeg.animations.add('flap', [0, 1, 2, 3], 10, true);
+    beerkeg.inputEnabled = true;
+    beerkeg.body.collideWorldBounds = true;
 
+    beerkeg.body.allowGravity = false;
+    beerkeg.angle = 0;
+    beerkeg.reset(game.world.width / 4, game.world.height / 2);
+    beerkeg.scale.setTo(2, 2);
+    beerkeg.animations.play('flap');
+
+    return beerkeg;
 }
 
 })();
