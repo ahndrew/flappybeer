5d510f64794feb2a4f2a44e841a2aa97d872ce8d
Francisco Gutierrez <fgutierrez@thrillist.com>
2014-02-28 15:56:14 -0500

    found a full flappy bird phaserjs repo

--- public/js/main.js
+++ public/js/main.js
@@ -1,145 +1,407 @@
-(function(){
+var DEBUG = false;
+var SPEED = 690;
+var GRAVITY = 40;
+var FLAP = 620;
+var SPAWN_RATE = 1 / 1.2;
+var OPENING = 134;
 
-var game = new Phaser.Game(512, 512, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+WebFontConfig = {
+    google: { families: [ 'Press+Start+2P::latin' ] },
+    active: main
+};
+(function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+})(); 
+
+
+function main() {
+
+var state = {
+    preload: preload,
+    create: create,
+    update: update,
+    render: render
+};
+
+var parent = document.querySelector('#screen');
+
+var game = new Phaser.Game(
+    0,
+    0,
+    Phaser.CANVAS,
+    parent,
+    state,
+    false,
+    false
+);
 
 function preload() {
-
-    game.load.image('sky', '/images/sky2.png');
-    game.load.image('ground', '/images/platform.png');
-    game.load.image('star', '/images/star.png');
-    game.load.spritesheet('dude', '/images/dude.png', 32, 48);
-
-    game.load.audio('boden', ['/audio/budligt_real_chinese.mp3']);
-
+    var assets = {
+        spritesheet: {
+            birdie: ['/assets/birdie.png', 24, 24],
+            clouds: ['/assets/clouds.png', 128, 64]
+        },
+        image: {
+            finger: ['/assets/finger.png'],
+            fence: ['/assets/fence.png']
+        },
+        audio: {
+            flap: ['/assets/flap.wav'],
+            score: ['/assets/score.wav'],
+            hurt: ['/assets/hurt.wav']
+        }
+    };
+    Object.keys(assets).forEach(function(type) {
+        Object.keys(assets[type]).forEach(function(id) {
+            game.load[type].apply(game.load, [id].concat(assets[type][id]));
+        });
+    });
 }
 
-var player;
-var platforms;
-var cursors;
-
-var stars;
-var score = 0;
-var scoreText;
+var gameStarted,
+    gameOver,
+    score,
+    bg,
+    credits,
+    clouds,
+    fingers,
+    invs,
+    birdie,
+    fence,
+    scoreText,
+    instText,
+    gameOverText,
+    flapSnd,
+    scoreSnd,
+    hurtSnd,
+    fingersTimer,
+    cloudsTimer;
 
 function create() {
+    // Set world dimensions
+    var screenWidth = parent.clientWidth > window.innerWidth ? window.innerWidth : parent.clientWidth;
+    var screenHeight = parent.clientHeight > window.innerHeight ? window.innerHeight : parent.clientHeight;
+    game.world.width = screenWidth;
+    game.world.height = screenHeight;
+    // Draw bg
+    bg = game.add.graphics(0, 0);
+    bg.beginFill(0xDDEEFF, 1);
+    bg.drawRect(0, 0, game.world.width, game.world.height);
+    bg.endFill();
+    // Credits 'yo
+    credits = game.add.text(
+        game.world.width / 2,
+        10,
+        '',
+        {
+            font: '8px "Press Start 2P"',
+            fill: '#fff',
+            align: 'center'
+        }
+    );
+    credits.anchor.x = 0.5;
+    // Add clouds group
+    clouds = game.add.group();
+    // Add fingers
+    fingers = game.add.group();
+    // Add invisible thingies
+    invs = game.add.group();
+    // Add birdie
+    birdie = game.add.sprite(0, 0, 'birdie');
+    birdie.anchor.setTo(0.5, 0.5);
+    birdie.animations.add('fly', [0, 1, 2, 3], 10, true);
+    birdie.inputEnabled = true;
+    birdie.body.collideWorldBounds = true;
+    birdie.body.gravity.y = GRAVITY;
+    // Add fence
+    fence = game.add.tileSprite(0, game.world.height - 32, game.world.width, 32, 'fence');
+    fence.tileScale.setTo(2, 2);
+    // Add score text
+    scoreText = game.add.text(
+        game.world.width / 2,
+        game.world.height / 4,
+        "",
+        {
+            font: '16px "Press Start 2P"',
+            fill: '#fff',
+            stroke: '#430',
+            strokeThickness: 4,
+            align: 'center'
+        }
+    );
+    scoreText.anchor.setTo(0.5, 0.5);
+    // Add instructions text
+    instText = game.add.text(
+        game.world.width / 2,
+        game.world.height - game.world.height / 4,
+        "",
+        {
+            font: '8px "Press Start 2P"',
+            fill: '#fff',
+            stroke: '#430',
+            strokeThickness: 4,
+            align: 'center'
+        }
+    );
+    instText.anchor.setTo(0.5, 0.5);
+    // Add game over text
+    gameOverText = game.add.text(
+        game.world.width / 2,
+        game.world.height / 2,
+        "",
+        {
+            font: '16px "Press Start 2P"',
+            fill: '#fff',
+            stroke: '#430',
+            strokeThickness: 4,
+            align: 'center'
+        }
+    );
+    gameOverText.anchor.setTo(0.5, 0.5);
+    gameOverText.scale.setTo(2, 2);
+    // Add sounds
+    flapSnd = game.add.audio('flap');
+    scoreSnd = game.add.audio('score');
+    hurtSnd = game.add.audio('hurt');
+    // Add controls
+    game.input.onDown.add(flap);
+    // Start clouds timer
+    cloudsTimer = new Phaser.Timer(game);
+    cloudsTimer.onEvent.add(spawnCloud);
+    cloudsTimer.start();
+    cloudsTimer.add(Math.random());
+    // RESET!
+    reset();
+}
 
-    music = game.add.audio('boden');
-    //music.play();
+function reset() {
+    gameStarted = false;
+    gameOver = false;
+    score = 0;
+    credits.renderable = true;
+    scoreText.setText("DON'T\nTOUCH\nMY\nBIRDIE");
+    instText.setText("TOUCH TO FLAP\nBIRDIE WINGS");
+    gameOverText.renderable = false;
+    birdie.body.allowGravity = false;
+    birdie.angle = 0;
+    birdie.reset(game.world.width / 4, game.world.height / 2);
+    birdie.scale.setTo(2, 2);
+    birdie.animations.play('fly');
+    fingers.removeAll();
+    invs.removeAll();
+}
 
-    //  A simple background for our game
-    //game.add.sprite(0, 0, 'sky');
+function start() {
+    credits.renderable = false;
+    birdie.body.allowGravity = true;
+    // SPAWN FINGERS!
+    fingersTimer = new Phaser.Timer(game);
+    fingersTimer.onEvent.add(spawnFingers);
+    fingersTimer.start();
+    fingersTimer.add(2);
+    // Show score
+    scoreText.setText(score);
+    instText.renderable = false;
+    // START!
+    gameStarted = true;
+}
 
-    bgtile = game.add.tileSprite(0, 0, game.stage.bounds.width, game.cache.getImage('sky').height, 'sky');
-
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = game.add.group();
-
-    // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
-
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
-
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
-
-    //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
-    ledge.body.immovable = true;
-
-    ledge = platforms.create(-150, 250, 'ground');
-    ledge.body.immovable = true;
-
-    // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
-
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 1000;
-    player.body.collideWorldBounds = true;
-
-    //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-    /*
-    //  Finally some stars to collect
-    stars = game.add.group();
-
-    //  Here we'll create 12 of them evenly spaced apart
-    for (var i = 0; i < 12; i++)
-    {
-        //  Create a star inside of the 'stars' group
-        var star = stars.create(i * 70, 0, 'star');
-
-        //  Let gravity do its thing
-        star.body.gravity.y = 1200;
-
-        //  This just gives each star a slightly random bounce value
-        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+function flap() {
+    if (!gameStarted) {
+        start();
     }
-*/
-    //  The score
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    if (!gameOver) {
+        birdie.body.velocity.y = -FLAP;
+        flapSnd.play();
+    }
+}
 
-    //  Our controls.
-    cursors = game.input.keyboard.createCursorKeys();
-    
+function spawnCloud() {
+    cloudsTimer.stop();
+
+    var cloudY = Math.random() * game.height / 2;
+    var cloud = clouds.create(
+        game.width,
+        cloudY,
+        'clouds',
+        Math.floor(4 * Math.random())
+    );
+    var cloudScale = 2 + 2 * Math.random();
+    cloud.alpha = 2 / cloudScale;
+    cloud.scale.setTo(cloudScale, cloudScale);
+    cloud.body.allowGravity = false;
+    cloud.body.velocity.x = -SPEED / cloudScale;
+    cloud.anchor.y = 0;
+
+    cloudsTimer.start();
+    cloudsTimer.add(4 * Math.random());
+}
+
+function o() {
+    return OPENING + 60 * ((score > 50 ? 50 : 50 - score) / 50);
+}
+
+function spawnFinger(fingerY, flipped) {
+    var finger = fingers.create(
+        game.width,
+        fingerY + (flipped ? -o() : o()) / 2,
+        'finger'
+    );
+    finger.body.allowGravity = false;
+
+    // Flip finger! *GASP*
+    finger.scale.setTo(2, flipped ? -2 : 2);
+    finger.body.offset.y = flipped ? -finger.body.height * 2 : 0;
+
+    // Move to the left
+    finger.body.velocity.x = -SPEED;
+
+    return finger;
+}
+
+function spawnFingers() {
+    fingersTimer.stop();
+
+    var fingerY = ((game.height - 16 - o() / 2) / 2) + (Math.random() > 0.5 ? -1 : 1) * Math.random() * game.height / 6;
+    // Bottom finger
+    var botFinger = spawnFinger(fingerY);
+    // Top finger (flipped)
+    var topFinger = spawnFinger(fingerY, true);
+
+    // Add invisible thingy
+    var inv = invs.create(topFinger.x + topFinger.width, 0);
+    inv.width = 2;
+    inv.height = game.world.height;
+    inv.body.allowGravity = false;
+    inv.body.velocity.x = -SPEED;
+
+    fingersTimer.start();
+    fingersTimer.add(1 / SPAWN_RATE);
+}
+
+function addScore(_, inv) {
+    invs.remove(inv);
+    score += 1;
+    scoreText.setText(score);
+    scoreSnd.play();
+}
+
+function setGameOver() {
+    gameOver = true;
+    instText.setText("TOUCH BIRDIE\nTO TRY AGAIN");
+    instText.renderable = true;
+    var hiscore = window.localStorage.getItem('hiscore');
+    hiscore = hiscore ? hiscore : score;
+    hiscore = score > parseInt(hiscore, 10) ? score : hiscore;
+    window.localStorage.setItem('hiscore', hiscore);
+    gameOverText.setText("GAMEOVER\n\nHIGH SCORE\n" + hiscore);
+    gameOverText.renderable = true;
+    // Stop all fingers
+    fingers.forEachAlive(function(finger) {
+        finger.body.velocity.x = 0;
+    });
+    invs.forEach(function(inv) {
+        inv.body.velocity.x = 0;
+    });
+    // Stop spawning fingers
+    fingersTimer.stop();
+    // Make birdie reset the game
+    birdie.events.onInputDown.addOnce(reset);
+    hurtSnd.play();
 }
 
 function update() {
-
-     bgtile.tilePosition.x -= 10;
-
-    //  Collide the player and the stars with the platforms
-    game.physics.collide(player, platforms);
-    game.physics.collide(stars, platforms);
-
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    game.physics.overlap(player, stars, collectStar, null, this);
-
-    //  Reset the players velocity (movement)
-    player.body.velocity.x = 0;
-
-    if (cursors.left.isDown)
-    {
-        //  Move to the left
-        player.body.velocity.x = -150;
-
-        player.animations.play('left');
+    if (gameStarted) {
+        // Make birdie dive
+        var dvy = FLAP + birdie.body.velocity.y;
+        birdie.angle = (90 * dvy / FLAP) - 180;
+        if (birdie.angle < -30) {
+            birdie.angle = -30;
+        }
+        if (
+            gameOver ||
+            birdie.angle > 90 ||
+            birdie.angle < -90
+        ) {
+            birdie.angle = 90;
+            birdie.animations.stop();
+            birdie.frame = 3;
+        } else {
+            birdie.animations.play('fly');
+        }
+        // Birdie is DEAD!
+        if (gameOver) {
+            if (birdie.scale.x < 4) {
+                birdie.scale.setTo(
+                    birdie.scale.x * 1.2,
+                    birdie.scale.y * 1.2
+                );
+            }
+            // Shake game over text
+            gameOverText.angle = Math.random() * 5 * Math.cos(game.time.now / 100);
+        } else {
+            // Check game over
+            game.physics.overlap(birdie, fingers, setGameOver);
+            if (!gameOver && birdie.body.bottom >= game.world.bounds.bottom) {
+                setGameOver();
+            }
+            // Add score
+            game.physics.overlap(birdie, invs, addScore);
+        }
+        // Remove offscreen fingers
+        fingers.forEachAlive(function(finger) {
+            if (finger.x + finger.width < game.world.bounds.left) {
+                finger.kill();
+            }
+        });
+        // Update finger timer
+        fingersTimer.update();
+    } else {
+        birdie.y = (game.world.height / 2) + 8 * Math.cos(game.time.now / 200);
     }
-    else if (cursors.right.isDown)
-    {
-        //  Move to the right
-        player.body.velocity.x = 150;
-
-        player.animations.play('right');
+    if (!gameStarted || gameOver) {
+        // Shake instructions text
+        instText.scale.setTo(
+            2 + 0.1 * Math.sin(game.time.now / 100),
+            2 + 0.1 * Math.cos(game.time.now / 100)
+        );
     }
-    else
-    {
-        //  Stand still
-        player.animations.stop();
-
-        player.frame = 4;
+    // Shake score text
+    scoreText.scale.setTo(
+        2 + 0.1 * Math.cos(game.time.now / 100),
+        2 + 0.1 * Math.sin(game.time.now / 100)
+    );
+    // Update clouds timer
+    cloudsTimer.update();
+    // Remove offscreen clouds
+    clouds.forEachAlive(function(cloud) {
+        if (cloud.x + cloud.width < game.world.bounds.left) {
+            cloud.kill();
+        }
+    });
+    // Scroll fence
+    if (!gameOver) {
+        fence.tilePosition.x -= game.time.physicsElapsed * SPEED / 2;
     }
-    
-    //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-        player.body.velocity.y = -650;
-    }
-
 }
 
-function collectStar (player, star) {
-    
-    // Removes the star from the screen
-    star.kill();
-
-    //  Add and update the score
-    score += 10;
-    scoreText.content = 'Score: ' + score;
-
+function render() {
+    if (DEBUG) {
+        game.debug.renderSpriteBody(birdie);
+        fingers.forEachAlive(function(finger) {
+            game.debug.renderSpriteBody(finger);
+        });
+        invs.forEach(function(inv) {
+            game.debug.renderSpriteBody(inv);
+        });
+    }
 }
 
-})();
+};
